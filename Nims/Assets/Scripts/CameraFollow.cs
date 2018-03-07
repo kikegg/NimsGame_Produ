4a8e83c7825a19387022de5fc830de99d88ead00
kikegg <quique23d@hotmail.com>
2018-03-07 01:09:41 +0100

    P1AvanceUnity
    Con Blink incluido

---
+++ Nims/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    TargetJoint2D tj;
+    GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        tj = GetComponent<TargetJoint2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        tj.target = (Vector2)player.transform.position;
+	}
+}
