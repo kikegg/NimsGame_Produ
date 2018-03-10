c2207cef5412b38b4da09b4d0026e02b0e33436e
kikegg <quique23d@hotmail.com>
2018-03-10 02:39:09 +0100

    GranAvanceUnity

---
+++ NimsGame/Assets/Scripts/Menus.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menus : MonoBehaviour {
+
+	public GameObject pause;
+	private bool paused = false;
+
+	void Start(){
+		pause.SetActive (false);
+	}
+
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			paused = !paused;
+		}	
+		if (paused) {
+			pause.SetActive (true);
+			Time.timeScale = 0;
+		}
+		if (!paused) {
+			pause.SetActive (false);
+			Time.timeScale = 1;
+		}
+	}
+
+	public void Continue (){
+		paused = false;
+		pause.SetActive (false);
+		Time.timeScale = 1;
+
+	}
+
+	public void Restart(){
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+
+	public void Menu(){
+		SceneManager.LoadScene ("Inicio");
+		paused = false;
+	}
+
+	public void Quit(){
+		Application.Quit ();
+	}
+}
